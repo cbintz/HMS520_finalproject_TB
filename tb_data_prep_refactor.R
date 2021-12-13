311f3237da36bad2d2a1102fd4e12fe4aedd0b2d
Corinne Bintz <bintzc@Corinnes-MacBook-Pro.local>
2021-12-13 10:28:09 -0800

    commiting final changes

--- tb_data_prep_refactor.R
+++ tb_data_prep_refactor.R
@@ -12,8 +12,6 @@ pacman::p_load(tidyverse, data.table)
 indir <- 'Desktop/tb_data/input_data'
 outdir <- 'Desktop/tb_data/output_data'
 
-## Read in all data files with columns of interest
-
 #  Function to clean world bank datasets
 clean_wb <- function(dataset, varname){
   data_s <- fread(file.path(indir, dataset))[,-c("Series Name", "Series Code", "Country Name")]
@@ -25,6 +23,7 @@ clean_wb <- function(dataset, varname){
   return(data_long)
 }
 
+## Read in all data files with columns of interest
 ## From WHO
 mdr_burden <- fread(file.path(indir, "WHO_MDR_RR_TB_burden_estimates_2020-07-23.csv"))[,c('iso3','e_rr_pct_new', 'year')]
 
@@ -42,23 +41,23 @@ health_funding_data_long <- clean_wb("world_bank_health_spending_per_capita.csv"
 private_funding_data_long <- clean_wb("private_percent.csv", "domestic_private_funding_share")
 
 ## From IHME
-ihme_tb_funding <- fread(file.path(indir, "ihme_tb.csv"))
+ihme_tb_funding <- fread(file.path(indir, "ihme_tb.csv"))[,-c("location_id", "location_name")]
 
 # merge all data together
-tb_funding_data <- Reduce(function(x, y) merge(x, y, by = c("iso3", "year"), all=TRUE), list(mdr_burden, tb_burden, tb_notifications, tb_lab_expenditure, pop_dens_long,health_funding_data_long,  private_funding_data_long,ihme_tb_funding  ))
+tb_funding_data <- Reduce(function(x, y) merge(x, y, by = c("iso3", "year"), all=TRUE), list(tb_notifications, mdr_burden, tb_burden, tb_lab_expenditure, pop_dens_long,health_funding_data_long,  private_funding_data_long,ihme_tb_funding  ))
 tb_funding_data <- merge(tb_funding_data, wealth_long, by = "iso3", all = TRUE)
 
 # filter to just be level 3 locations
 locs <- fread(file.path(indir, 'locs.csv'))[,c("location_name", "ihme_loc_id", "location_id")]
 length(unique(tb_funding_data$iso3))
 setdiff(locs$ihme_loc_id, tb_funding_data$iso3) # missing taiwan 
-intersect(colnames(locs), colnames(tb_funding_data))
-tb_funding_data <- merge(locs, tb_funding_data, by.x = c("ihme_loc_id","location_name", "location_id"), by.y = c("iso3", "location_name", "location_id"))
+tb_funding_data <- merge(tb_funding_data,locs, by.x= "iso3", by.y = "ihme_loc_id")
 
 # add on population estimates for calculating lab expenditure per capita and confirmed mdr per capita 
 pop <- fread(file.path(indir, 'pop.csv'))
 colnames(pop)[4] <- "year"
-tb_funding_data <- merge(tb_funding_data, pop, by = c("location_id", "year"), )
+#pop_locs <- merge(pop, locs[, c("location_id", "ihme_loc_id")], by = "location_id")
+tb_funding_data <- merge(tb_funding_data, pop, by.x = c("year", "location_id"), by.y = c( "year", "location_id") )
 
 # replace .. with NA
 tb_funding_data[tb_funding_data == ".."] <- NA
@@ -74,8 +73,7 @@ tb_funding_data <- tb_funding_data[,-c("V1","run_id", "sex_id", "location_id")]
 tb_funding_data[, time := year - 2014]
 
 # compute median wealth per capita
-median_wealth <- median(tb_funding_data$total_wealth_per_capita)
+median_wealth <- median(tb_funding_data$total_wealth_per_capita, na.rm=T)
 tb_funding_data[, compare_median := as.factor(ifelse(total_wealth_per_capita >= median_wealth, "Above median total wealth per capita", "Below median total wealth per capita"))]
-
 # save prepared dataset
 write.csv(tb_funding_data, file.path(outdir, 'prepped_data.csv'), row.names = FALSE)
--- tb_visualizations_refactor.R
+++ tb_visualizations_refactor.R
@@ -6,7 +6,7 @@
 rm(list=ls())
 
 ## Install packages and load libraries
-pacman::p_load(tidyverse, data.table, ggplot2, gridExtra)
+pacman::p_load(tidyverse, data.table, ggplot2, gridExtra,scales)
 
 ## Define directories
 indir <- 'Desktop/tb_data/output_data'
@@ -15,13 +15,19 @@ outdir <- 'Desktop/tb_data/visualizations'
 ## Read in prepped data file
 tb_df <- fread(file.path(indir, "prepped_data.csv"))
 
+## write function to save figure
+save_figure <- function(figure, figurename) {
+ 
+  ggsave(file.path(outdir, paste0(figurename, ".pdf")), width = 8, height = 8)
+  
+}
+
 ## Read in model results
 results_mdr_exp_lab_time2 <- fread(file.path(indir, 'conf_lab_exp.csv'))
 results_mdr_exp_lab_time_above <- fread(file.path(indir, 'conf_lab_exp_above.csv'))
 results_mdr_exp_lab_time_below <- fread(file.path(indir, 'conf_lab_exp_below.csv'))
 
 median_wealth <- median(tb_df$total_wealth_per_capita)
-colnames(tb_df)
 
 # Let's graph estimated % of new TB cases with rifampicin resistant TB as a function of total wealth per capita
 conf_rrmdr_wealth <- ggplot(data = tb_df, aes(x = total_wealth_per_capita, y = e_rr_pct_new))+ 
@@ -29,11 +35,12 @@ conf_rrmdr_wealth <- ggplot(data = tb_df, aes(x = total_wealth_per_capita, y = e
   geom_vline(xintercept = median_wealth, size=0.5) + 
   geom_smooth(method=lm, color = "black") + 
   xlab("Total wealth per capita") + 
-  ylab("Estimated % of new TB cases with rifampicin resistant TB") 
+  ylab("Estimated % of new TB cases with rifampicin resistant TB") +
   ggtitle("Number of confirmed rr tb cases by total wealth per capita") +
   theme_bw() +
   labs(color = "Relation to global median total wealth per capita")+
   theme(panel.grid = element_blank())
+save_figure(conf_rrmdr_wealth, 'conf_rrmdr_wealth')
 
 # Let's look closer at countries below the median income
 below <- ggplot(data = tb_df[compare_median == "Below median total wealth per capita"], aes(x = total_wealth_per_capita, y = e_rr_pct_new))+ 
@@ -45,8 +52,11 @@ below <- ggplot(data = tb_df[compare_median == "Below median total wealth per ca
   ylab("Estimated % of new TB cases with rifampicin resistant TB") +
   ggtitle("Countries below the global median total wealth per capita") +
   theme_bw() +
+  scale_x_continuous(label = comma)+
   labs(color = "Relation to global median total wealth per capita")+
-  theme(panel.grid = element_blank())
+  theme(panel.grid = element_blank(), 
+        text  = element_text(size = 10))
+save_figure(below, 'below')
 
 # Let's look closer at countries above the median income
 above <- ggplot(data = tb_df[compare_median == "Above median total wealth per capita"], aes(x = total_wealth_per_capita, y = e_rr_pct_new))+ 
@@ -56,13 +66,17 @@ above <- ggplot(data = tb_df[compare_median == "Above median total wealth per ca
   geom_smooth(aes(x = total_wealth_per_capita, y = e_rr_pct_new), method=lm) + 
   xlab("Total wealth per capita") + 
   ylab("Estimated % of new TB cases with rifampicin resistant TB") +
+  scale_x_continuous(label = comma)+
   ggtitle("Countries above the global median total wealth per capita") +
   theme_bw() +
   labs(color = "Relation to global median total wealth per capita")+
-  theme(panel.grid = element_blank())
+  theme(panel.grid = element_blank(),
+        text  = element_text(size = 10))
+save_figure(above, 'above')
 
 #Let's graph these together
-above_below <- grid.arrange(below, above, ncol=2)
+above_below <- arrangeGrob(below, above, ncol=2)
+ggsave(file.path(outdir, "above_below.png"), above_below, height = 5.2, width = 10, dpi = 600)
 
 # Graph confirmed cases by laboratory expenditure 
 conf_lab_exp  <- ggplot(data = tb_df, aes(x = exp_lab, y = conf_rrmdr, color=location_name)) +
@@ -71,34 +85,43 @@ conf_lab_exp  <- ggplot(data = tb_df, aes(x = exp_lab, y = conf_rrmdr, color=loc
   geom_abline( data =results_mdr_exp_lab_time2, aes(intercept=`(Intercept)`, slope=exp_lab)) +
   theme(legend.position = "none") +
   ggtitle("Laboratory expenditure and cases of RR-TB or MDR-TB")+
+  scale_x_continuous(label = comma)+
   theme_bw() +
   theme(panel.grid = element_blank(),
         legend.position = "none")
+save_figure(conf_lab_exp, 'conf_lab_exp')
 
 # Graph confirmed cases by laboratory expenditure: above median 
-conf_lab_exp  <- ggplot(data = tb_df, aes(x = exp_lab, y = conf_rrmdr, color=location_name)) +
-  labs(x=" Actual expenditure on laboratory infrastructure, equipment and supplies (US Dollars)",y="Number of Laboratory-Confirmed RR-TB or MDR-TB cases")+
+conf_lab_exp_above  <- ggplot(data = tb_df[compare_median == "Above median total wealth per capita"], aes(x = exp_lab, y = conf_rrmdr, color=location_name)) +
+  labs(x=" Actual expenditure on laboratory infrastructure, equipment and supplies \n (US Dollars)",y="Number of Laboratory-Confirmed RR-TB or MDR-TB cases")+
   geom_point(shape = 16, size=1.8) +
   geom_abline( data =results_mdr_exp_lab_time_above, aes(intercept=`(Intercept)`, slope=exp_lab)) +
   theme(legend.position = "none") +
-  ggtitle("Cases of RR-TB or MDR-TB by laboratory expenditure, locations above median total wealth per capita")+
+  scale_x_continuous(label = comma)+
+  ggtitle("Cases of RR-TB or MDR-TB by laboratory expenditure,\n locations above median total wealth per capita")+
   theme_bw() +
   theme(panel.grid = element_blank(),
-        legend.position = "none")
+        legend.position = "none", 
+        text  = element_text(size = 10))
+save_figure(conf_lab_exp_above, 'conf_lab_exp_above')
 
-# fix x axis label
 # Graph confirmed cases by laboratory expenditure: below median 
-conf_lab_exp  <- ggplot(data = tb_df, aes(x = exp_lab, y = conf_rrmdr, color=location_name)) +
-  labs(x=" Actual expenditure on laboratory infrastructure, equipment and supplies (US Dollars)",y="Number of Laboratory-Confirmed RR-TB or MDR-TB cases")+
+conf_lab_exp_below  <- ggplot(data = tb_df[compare_median == "Below median total wealth per capita"], aes(x = exp_lab, y = conf_rrmdr, color=location_name)) +
+  labs(x=" Actual expenditure on laboratory infrastructure, equipment and supplies \n (US Dollars)",y="Number of Laboratory-Confirmed RR-TB or MDR-TB cases")+
   geom_point(shape = 16, size=1.8) +
   geom_abline( data =results_mdr_exp_lab_time_below, aes(intercept=`(Intercept)`, slope=exp_lab)) +
   theme(legend.position = "none") +
-  ggtitle("Cases of RR-TB or MDR-TB by laboratory expenditure, locations below median total wealth per capita")+
-  theme_bw() +
+  ggtitle("Cases of RR-TB or MDR-TB by laboratory expenditure,\n locations below median total wealth per capita")+
+  theme_bw() + 
+  scale_x_continuous(label = comma)+
   theme(panel.grid = element_blank(),
-        legend.position = "none")
+        legend.position = "none", 
+        text  = element_text(size = 10))
+save_figure(conf_lab_exp_below, 'conf_lab_exp_below')
 
-
+#Let's graph these together
+above_below_conf_rrmdr <- arrangeGrob(conf_lab_exp_below, conf_lab_exp_above, ncol=2)
+ggsave(file.path(outdir, "above_below_conf_rrmdr.png"), above_below_conf_rrmdr, height = 5.2, width = 10, dpi = 600)
 
 
 
