bcfac34b858e8941354d1fc5c0f0080d6db65729
Corinne Bintz <bintzc@Corinnes-MacBook-Pro.local>
2021-12-14 09:44:09 -0800

    added empty template script to explore new datasets

---
+++ tb_explore_data_refactor.R
@@ -0,0 +1,19 @@
+## Author: Corinne Bintz
+## Date: 12/7/21
+## Purpose: Template script to explore new datasets that may be added to the analyses 
+
+## clear memory
+rm(list=ls())
+
+## Install packages and load libraries
+pacman::p_load(tidyverse, data.table)
+
+## Define directories
+indir <- 'Desktop/tb_data/input_data'
+outdir <- 'Desktop/tb_data/output_data'
+
+## Read in new data
+
+## Diagnostic models 
+
+## Visualize
